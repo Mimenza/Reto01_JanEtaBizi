dfd2d061b8db013bc0ec13ee3da329c11c81dc97
Julen <julenarmeex@gmail.com>
2021-10-06 13:16:44 +0200

    Splash screen creado

--- app/src/main/java/com/example/reto01/MainActivity.kt
+++ app/src/main/java/com/example/reto01/MainActivity.kt
@@ -6,10 +6,16 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
+
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
+
+
 
         btn_0login.setOnClickListener(){
             val i = Intent(this@MainActivity, activity_1login::class.java)
