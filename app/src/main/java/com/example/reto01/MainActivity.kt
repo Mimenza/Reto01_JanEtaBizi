1582c06644720e456cd5463d7f5b5a3f43d1bb38
Endika <dash6657@gmail.com>
2021-10-06 13:31:42 +0200

    resolved conflict

--- app/src/main/java/com/example/reto01/MainActivity.kt
+++ app/src/main/java/com/example/reto01/MainActivity.kt
@@ -16,15 +16,12 @@ class MainActivity : AppCompatActivity() {
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
         window.decorView.apply {
             //esconde el nav inferior
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
         }
-=======
-
->>>>>>> 6925ae535b464dd4491d97df9cedfa6c2106db12
+        
 
         btn_0login.setOnClickListener(){
             val i = Intent(this@MainActivity, activity_1login::class.java)
