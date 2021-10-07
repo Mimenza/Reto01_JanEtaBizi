26c544f082612da2b47ae66483a43e0a715f2239
Julen <julenarmeex@gmail.com>
2021-10-07 10:27:52 +0200

    no intro slider

--- app/src/main/java/com/example/reto01/SplashActivity.kt
+++ app/src/main/java/com/example/reto01/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.thesimplycoder.simpleintroslider.IntroSliderActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
