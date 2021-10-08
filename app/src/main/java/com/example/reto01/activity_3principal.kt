b3e29afaab21b48d5e0797dd1cc4855f9d6a716c
Galder <galdergomez50@gmail.com>
2021-10-08 13:08:45 +0200

    table removed

--- app/src/main/java/com/example/reto01/activity_3principal.kt
+++ app/src/main/java/com/example/reto01/activity_3principal.kt
@@ -16,7 +16,6 @@ class activity_3principal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_3principal)
-        init()
 
         fun navegacion_principal() {
             val navegacion_compra = Intent(this, activity_3principal::class.java)
@@ -67,59 +66,6 @@ class activity_3principal : AppCompatActivity() {
                 else -> false
             }
         }
-
-
-
-
     }
-
-    fun init() {
-        val stk = tl_3tabla as TableLayout
-        val tbrow0 = TableRow(this)
-        val tv0 = TextView(this)
-        tv0.text = " Sl.No "
-        tv0.setTextColor(Color.WHITE)
-        tbrow0.addView(tv0)
-        val tv1 = TextView(this)
-        tv1.text = " Product "
-        tv1.setTextColor(Color.WHITE)
-        tbrow0.addView(tv1)
-        val tv2 = TextView(this)
-        tv2.text = " Unit Price "
-        tv2.setTextColor(Color.WHITE)
-        tbrow0.addView(tv2)
-        val tv3 = TextView(this)
-        tv3.text = " Stock Remaining "
-        tv3.setTextColor(Color.WHITE)
-        tbrow0.addView(tv3)
-        stk.addView(tbrow0)
-        for (i in 0..24) {
-            val tbrow = TableRow(this)
-            val t1v = TextView(this)
-            t1v.text = "" + i
-            t1v.setTextColor(Color.WHITE)
-            t1v.gravity = Gravity.CENTER
-            tbrow.addView(t1v)
-            val t2v = TextView(this)
-            t2v.text = "Product $i"
-            t2v.setTextColor(Color.WHITE)
-            t2v.gravity = Gravity.CENTER
-            tbrow.addView(t2v)
-            val t3v = TextView(this)
-            t3v.text = "Rs.$i"
-            t3v.setTextColor(Color.WHITE)
-            t3v.gravity = Gravity.CENTER
-            tbrow.addView(t3v)
-            val t4v = TextView(this)
-            t4v.text = "" + i * 15 / 32 * 10
-            t4v.setTextColor(Color.WHITE)
-            t4v.gravity = Gravity.CENTER
-            tbrow.addView(t4v)
-            stk.addView(tbrow)
-        }
-    }
-
-
-
 }
 
