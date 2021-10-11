6775a21feae3638ea9a96e79c7b6f67409f8c752
ALBA <olanoalba@gmail.com>
2021-10-11 10:48:14 +0200

    pantalla gestion admin usuarios

---
+++ app/src/main/java/com/example/reto01/activity_7_02productos.kt
@@ -0,0 +1,11 @@
+package com.example.reto01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_7_02productos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_7_02productos)
+    }
+}
