052715c14f0e6bbbbebfe99c5882da00fd91669f
ALBA <olanoalba@gmail.com>
2021-10-13 09:52:38 +0200

    pantalla gestion pedido, actualizar fotos

--- app/src/main/java/com/example/reto01/activity_7_03pedidos.kt
+++ app/src/main/java/com/example/reto01/activity_7_03pedidos.kt
@@ -3,22 +3,16 @@ package com.example.reto01
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_7_02productos.*
 
-class activity_7_02productos : AppCompatActivity() {
+class activity_7_03pedidos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_7_02productos)
+        setContentView(R.layout.activity_7_03pedidos)
         getSupportActionBar()?.hide()
-        val lista = arrayOf("PRODUCTO 1", "PRODUCTO 2", "PRODUCTO 3", "PRODUCTO 4","CREAR NUEVO PRODUCTO")
-        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista)
-        spinner_productos.adapter = adaptador
-
         imgv7_02_atras.setOnClickListener() {
             val i = Intent(this, activity_7admin::class.java)
             startActivity(i)
         }
-
     }
 }
