93c5d531fa1b2e7e9d7b9d9d8e796705e8ae3a87
mimenza <dash6657@gmail.com>
2021-10-24 14:31:58 +0200

    order and pedido_producto working

--- app/src/main/java/com/example/reto01/Model/Order.kt
+++ app/src/main/java/com/example/reto01/Model/Order.kt
@@ -3,7 +3,7 @@ package com.example.reto01.Model
 import java.util.*
 
 data class Order(
-    val id_order: Int? = null,
+    var id_order: Int? = null,
     var id_user: Int? = null,
     var date: String? = null,
     var total: Double? = null,
