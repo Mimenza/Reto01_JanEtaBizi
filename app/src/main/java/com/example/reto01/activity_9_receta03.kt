2fcb8c4611bb247058d043dab8ccce84d5f4f363
ALBA <olanoalba@gmail.com>
2021-10-25 12:38:21 +0200

    diseño blog

---
+++ app/src/main/java/com/example/reto01/activity_9_receta03.kt
@@ -0,0 +1,18 @@
+package com.example.reto01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_7_2_2producto.*
+
+class activity_9_receta03 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_9_receta03)
+        getSupportActionBar()?.hide()
+        imgv7_02_atras.setOnClickListener() {
+            val i = Intent(this, activity_9blog::class.java)
+            startActivity(i)
+        }
+    }
+}
