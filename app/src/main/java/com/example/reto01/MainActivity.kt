dd355e73361771d8ffcd217559ef8a92a1acdf63
Galder <galdergomez50@gmail.com>
2021-11-02 12:56:34 +0100

    coroutine

--- app/src/main/java/com/example/reto01/MainActivity.kt
+++ app/src/main/java/com/example/reto01/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
 
-        Handler().postDelayed({
-            val intent = Intent(this, activity_1login::class.java)
-            startActivity(intent)
-            this.overridePendingTransition(0, 0)
-            finish()
-        }, 2000) // 2000 is the delayed time in milliseconds.
+        runBlocking {
+            launch {
+                Handler().postDelayed({
+                    val intent = Intent(this@MainActivity, activity_1login::class.java)
+                    startActivity(intent)
+                    this@MainActivity.overridePendingTransition(0, 0)
+                    finish()
+                }, 2000) // 2000 is the delayed time in milliseconds.
+            }
+        }
     }
 }
